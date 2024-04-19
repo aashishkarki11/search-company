a92d381ea9c272b80cb7a6682211bcaffda0b650
aashishsursa <aashish.karki@sursatech.com>
2024-04-19 21:21:26 +0545

    feat : deployed code in vercel

--- server.js
+++ server.js
@@ -1,47 +1,38 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 const companyData = require('./data.json');
 
-app.use(cors());
+app.use(cors({
+    origin: '*',  
+    methods: 'GET,POST,PUT,DELETE', 
+    credentials: true 
+}));
+
 app.use(express.json());
 
 app.get('/autocomplete', (req, res) => {
     const searchTerm = req.query.term.toLowerCase();
-    console.log("search term: " + searchTerm);
     const suggestions = companyData.companies
         .filter(company => company.name.toLowerCase().includes(searchTerm))
         .map(company => company.name);
-
-    console.log(suggestions);
 
     res.json(suggestions);
 });
 
 app.get('/company/:name', (req, res) => {
-    console.log("Inside controller");
+    const companyName = req.params.name.replace(/\s/g, '').toLowerCase();
+    const company = companyData.companies.find(c => c.name.replace(/\s/g, '').toLowerCase() === companyName);
 
-    const companyName = req.params.name.replace(/\s/g, '');
-    console.log("Company Name:", companyName);
-    console.log("Company Data:", companyData);
-
-    const company = companyData.companies.find(company => company.name.replace(/\s/g, '').toLowerCase() === companyName.toLowerCase());
-
-    if (!company) {
-        console.log("Company not found");
-        return res.status(404).json({ error: 'Company not found' });
+    if (company) {
+        res.json(company);
+    } else {
+        res.status(404).json({ error: 'Company not found' }); 
     }
-
-    console.log("Company found:", company);
-    res.json(company);
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-app.use(cors({
-    origin: 'http://127.0.0.1:5500/index.html'
-}));
